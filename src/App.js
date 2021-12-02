6040c9e3ffe746ea42df88b0e0c88c8591cfe85a
pavstar619 <pavelx16@gmail.com>
2021-12-02 14:16:56 +0600

    ss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
 function App(props) {
+  const taskList = props.tasks.map(task => (<Todo name={task.name} completed={task.completed} id={task.id} key={task.id}/>));
+  // console.log(taskList)
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -22,21 +26,9 @@ function App(props) {
         </button>
       </form>
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id="list-heading">
         3 tasks remaining
@@ -46,9 +38,11 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo />
-        <Todo />
-        <Todo />
+        {/* <Todo name="Task1" completed={true} id="todo-0"/>
+        <Todo name="Task2" completed={false} id="todo-1"/>
+        <Todo name="Task3" completed={false} id="todo-2"/> */}
+        {taskList}
+        
       
       </ul>
     </div>
